ab5bdb84750a5abd6671c558b44453b54556d978
hewsontrinh526 <6734@holbertonstudents.com>
2023-09-06 17:26:44 -0700

    Feat: Updating monty header

--- monty.h
+++ monty.h
@@ -11,6 +11,7 @@
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
+ *
  * @n: integer
  * @prev: points to the previous element of the stack (or queue)
  * @next: points to the next element of the stack (or queue)
